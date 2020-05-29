d4fd934e8e227f65ef6f22f27f14a43b94bd039e
trungld3 <k21tpm4@gmail.com>
2020-05-29 09:39:20 +0700

    Initial Commit

---
+++ Login Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Demo
+//
+//  Created by TrungLD on 5/29/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
